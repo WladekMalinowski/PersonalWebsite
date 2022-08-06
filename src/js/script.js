e89387362402ccfe813cf7c6bbc5681c64edbdfd
WladekMalinowski <109795134+WladekMalinowski@users.noreply.github.com>
2022-08-06 18:21:56 +0200

    js nav done

--- src/js/script.js
+++ src/js/script.js
@@ -6,3 +6,40 @@ window.addEventListener('scroll', e => {
     else navbar.remove(activeClass);
 
 });
+
+
+function btnCvActive() {
+    let btnCvA = document.getElementById('btnCv').classList;
+    let btnCvActive = 'btn_cv_active';
+    
+    btnCvA.add(btnCvActive);
+
+    function bntCvDis () {
+        btnCvA.remove(btnCvActive);
+    }
+
+    setTimeout(bntCvDis, 2000);
+
+    console.log('hi!');
+
+}
+
+setInterval(btnCvActive, 4000);
+
+function btnCvActiveNav() {
+    let btnCvA = document.getElementById('btnCvNav').classList;
+    let btnCvActive = 'btn_cv_active';
+    
+    btnCvA.add(btnCvActive);
+
+    function bntCvDis () {
+        btnCvA.remove(btnCvActive);
+    }
+
+    setTimeout(bntCvDis, 2000);
+
+    console.log('hi!');
+
+}
+
+setInterval(btnCvActiveNav, 4000);

68baf8b420db42ec995c7df0deccc445b36f1517
WladekMalinowski <109795134+WladekMalinowski@users.noreply.github.com>
2022-08-16 14:38:40 +0200

    before code review

--- src/js/script.js
+++ src/js/script.js
@@ -8,50 +8,23 @@ window.addEventListener('scroll', e => {
 });
 
 
-function btnCvActive() {
-    let btnCvA = document.getElementById('btnCv').classList;
-    let btnCvActive = 'btn_cv_active';
-    
-    btnCvA.add(btnCvActive);
 
-    function bntCvDis () {
-        btnCvA.remove(btnCvActive);
-    }
-    setTimeout(bntCvDis, 2000);
-}
+// let popupLinksClose = document.querySelector('.popup-close');
+// let popupBody = document.getElementById('popup').classList;
+// let popupActive = 'popup_open';
 
-setInterval(btnCvActive, 4000);
-
-function btnCvActiveNav() {
-    let btnCvA = document.getElementById('btnCvNav').classList;
-    let btnCvActive = 'btn_cv_active';
-    
-    btnCvA.add(btnCvActive);
-
-    setTimeout(function () {
-        btnCvA.remove(btnCvActive);
-    }, 2000);
-}
-
-setInterval(btnCvActiveNav, 4000);
+// setTimeout(function () {
+//     popupBody.add(popupActive);
+//     } , 100);
 
 
-let popupLinksClose = document.querySelector('.popup-close');
-let popupBody = document.getElementById('popup').classList;
-let popupActive = 'popup_open';
+// popupLinksClose.addEventListener('click', ()=> {
+//     popupBody.remove(popupActive);
+// });
 
-setTimeout(function () {
-    popupBody.add(popupActive);
-    } , 100);
-
-
-popupLinksClose.addEventListener('click', ()=> {
-    popupBody.remove(popupActive);
-});
-
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape') {
-        popupBody.remove(popupActive);
-    }
-});
+// document.addEventListener('keydown', function (e) {
+//     if (e.key === 'Escape') {
+//         popupBody.remove(popupActive);
+//     }
+// });
 
